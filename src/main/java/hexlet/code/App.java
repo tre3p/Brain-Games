349fa2757d8b23e01d3d37b61ad1b9106f0e40e1
k0damaDEV <68253357+k0damaDEV@users.noreply.github.com>
2021-07-13 00:31:07 +0300

    Update App.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -8,11 +8,6 @@ import hexlet.code.games.Prime;
 import java.util.Scanner;
 
 public class App {
-    private static final int FIRST_GAME = 2;
-    private static final int SECOND_GAME = 3;
-    private static final int THIRD_GAME = 4;
-    private static final int FOURTH_GAME = 5;
-    private static final int SIXTH_GAME = 6;
     public static void main(String[] args) {
         System.out.println("1 - Greet\n" + "2 - Even\n" + "3 - Calc\n" + "4 - GCD\n" + "5 - Progression\n"
                            + "6 - Prime\n" + "0 - Exit\n" + "Please enter the game number and press Enter.");
